4dab53ad6d0a2af8efd4533a7b806daa4df8bea6
nixxon111 <perfect07>
2016-01-05 17:17:20 +0100

    Follow smooth camera, Nixxon scene changes

---
+++ BBCTMA/Assets/Scripts/SwordmanAI.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwordmanAI : MonoBehaviour {
+
+    public bool goingLeft;
+    public bool goingRight;
+    private Animator animator;
+    private Rigidbody2D rigid;
+    private float scaleX;
+    private Transform player;
+    public float movementSpeed = 1f;
+
+    // Use this for initialization
+    void Start() {
+
+        rigid = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        player = GetComponent<Transform>();
+        scaleX = player.localScale.x;
+
+        goingRight = true;
+        player.localScale = new Vector3(scaleX, player.localScale.y, player.localScale.z);
+        rigid.velocity = new Vector2(1f * movementSpeed, rigid.velocity.y);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (goingRight)
+        {
+            player.localScale = new Vector3(scaleX, player.localScale.y, player.localScale.z);
+            rigid.velocity = new Vector2(1f * movementSpeed, rigid.velocity.y);
+            animator.SetBool("isWalking", true);
+        }
+        else if (goingLeft)
+        {
+            player.localScale = new Vector3(-scaleX, player.localScale.y, player.localScale.z);
+            rigid.velocity = new Vector2(-1f * movementSpeed, rigid.velocity.y);
+            animator.SetBool("isWalking", true);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collisionInfo)
+    {
+        if (collisionInfo.transform.tag == "Wall")
+        {
+            if (goingRight)
+            {
+                goingLeft = true;
+                goingRight = false;
+            }
+            else if (goingLeft)
+            {
+                goingLeft = false;
+                goingRight = true;
+            }
+        }
+    }
+}
