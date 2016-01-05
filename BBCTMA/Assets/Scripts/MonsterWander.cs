182f6847ed540f3d84a37d5e30c185c47009f49c
Søren Howe Gersager <syrelyre@gmail.com>
2016-01-05 19:04:20 +0100

    added enemy following player

--- BBCTMA/Assets/Scripts/MonsterWander.cs
+++ BBCTMA/Assets/Scripts/MonsterWander.cs
@@ -4,10 +4,12 @@ using System.Collections;
 public class MonsterWander : MonoBehaviour {
 	public float directionChangeInterval = 2f;
 	public float movementSpeed = 3f;
+	public Transform target;
+    public float range;
 	private Transform monster;
 	private Rigidbody2D rigid;
 	private float direction = 1f;
-
+    private bool seenEnemy = false;
 	// Use this for initialization
 	void Start () 
 	{
@@ -19,10 +21,24 @@ public class MonsterWander : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-		rigid.velocity = new Vector2(direction * movementSpeed, rigid.velocity.y);
+        var hit = Physics2D.Linecast(monster.position, target.position);
+        if (hit && hit.distance < range)
+		{
+                Debug.Log(hit.distance);
+                seenEnemy = true;
+                if (monster.position.x > target.position.x)
+                    direction = -1;
+                else
+                    direction = 1;
+		}
+		else
+		{
+            seenEnemy = false;
+            
+		}
+        rigid.velocity = new Vector2(direction * movementSpeed, rigid.velocity.y);
 		float angle = Mathf.Atan2(rigid.velocity.y, rigid.velocity.x) * Mathf.Rad2Deg;
 		monster.rotation = Quaternion.AngleAxis(angle, Vector3.down);
-
 
 	}
 
@@ -30,8 +46,12 @@ public class MonsterWander : MonoBehaviour {
 	{
 		while (true) 
 		{
-			newDirectionRoutine();
-			yield return new WaitForSeconds(directionChangeInterval);
+            if (!seenEnemy)
+            {
+                newDirectionRoutine();
+
+            }
+            yield return new WaitForSeconds(directionChangeInterval);
 		}
 	}
 
