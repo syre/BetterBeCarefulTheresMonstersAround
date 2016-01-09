79ffe8c972180eb28ed2b60d5a11b13f3239552a
Søren Howe Gersager <syrelyre@gmail.com>
2016-01-09 02:28:14 +0100

    added test equippable slots (inventory), and unfinished treasure loot script

---
+++ BBCTMA/Assets/Scripts/Inventory.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inventory : MonoBehaviour {
+
+    public GameObject[] itemSlots = new GameObject[4];
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+    public bool addItem(GameObject item)
+    {
+        for (int i = 0; i < itemSlots.Length; i++)
+        {
+            if (itemSlots[i] == null)
+            {
+                itemSlots[i] = item;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void dropItem(GameObject item)
+    {
+        for (int i = 0; i < itemSlots.Length; i++)
+        {
+            if (itemSlots[i] == item)
+            {
+                Debug.Log("item dropped");
+                itemSlots[i] = null;
+            }
+            
+        }
+        Debug.Log("could not find item to drop");
+    }
+    public void dropItem(int index)
+    {
+        if (itemSlots[index] != null)
+            itemSlots[index] = null;
+        else
+            Debug.Log("no item in slot "+index);
+    }
+}
