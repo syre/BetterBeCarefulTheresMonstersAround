b2e197e46213603137368db7c96a9eab398cea02
Søren Howe Gersager <syrelyre@gmail.com>
2016-01-09 14:49:56 +0100

    added LootView

--- BBCTMA/Assets/Scripts/InventoryGUI.cs
+++ BBCTMA/Assets/Scripts/InventoryGUI.cs
@@ -4,26 +4,7 @@ using System.Collections;
 public class InventoryGUI : MonoBehaviour {
 
     Inventory inventory;
-    void OnGUI()
-    {
-        GUI.Box(new Rect(10, 10, 300, 100), "Equipped Weapons");
-        if (GUI.Button(new Rect(40, 40, 50, 50), "1"))
-        {
-            inventory.dropItem(0);
-        }
-        else if (GUI.Button(new Rect(100, 40, 50, 50), "2"))
-        {
-            inventory.dropItem(1);
-        }
-        else if (GUI.Button(new Rect(160, 40, 50, 50), "3"))
-        {
-            inventory.dropItem(2);
-        }
-        else if (GUI.Button(new Rect(220, 40, 50, 50), "4"))
-        {
-            inventory.dropItem(3);
-        }
-    }
+
 
 	// Use this for initialization
 	void Start() 
--- BBCTMA/Assets/Scripts/TreasureLoot.cs
+++ BBCTMA/Assets/Scripts/TreasureLoot.cs
@@ -2,11 +2,10 @@
 using System.Collections;
 
 public class TreasureLoot : MonoBehaviour {
-
+    private GameObject LootView;
 	// Use this for initialization
 	void Start () 
 	{
-	
 	}
 	
 	// Update is called once per frame
@@ -20,7 +19,24 @@ public class TreasureLoot : MonoBehaviour {
         if (other.gameObject.tag == "Player")
         {
             Debug.Log("Loot for player!");
-            Destroy(gameObject);
+            if (!LootView)
+            {
+                Vector3 viewPos = transform.position + new Vector3(0f, 100f, 0f);
+                LootView = Instantiate(Resources.Load("UIElements/LootView"), viewPos, Quaternion.identity) as GameObject;
+                var canvas = UnityEngine.Object.FindObjectOfType<Canvas>();
+                LootView.transform.SetParent(canvas.transform, false);
+            }
+            else
+                LootView.SetActive(true);
+                
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            LootView.SetActive(false);
         }
     }
 }
