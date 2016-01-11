d84a8ed04ec4d9312e4a122d1ec475c98cb4d3de
Søren Howe Gersager <syrelyre@gmail.com>
2016-01-11 22:55:12 +0100

    added potion

---
+++ BBCTMA/Assets/Scripts/HpItem.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HpItem : MonoBehaviour {
+
+    public float hpGain = 10f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Healing Player");
+            var hpScript = other.gameObject.GetComponent<PlayerHealth>();
+            hpScript.addHealthPoints(hpGain);
+            Destroy(gameObject);
+        }
+    }
+}
